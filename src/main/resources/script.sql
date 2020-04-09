b303b8b452b8fc7f8932413bf164344a4494af08
ELLEUCH <ELLEUCH@DESKTOP-E28062K>
2020-04-09 10:28:21 +0200

    init project

---
+++ src/main/resources/script.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `Address` (
+  `id` int(11) unsigned NOT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `country` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `Customer` (
+  `id` int(11) unsigned NOT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
